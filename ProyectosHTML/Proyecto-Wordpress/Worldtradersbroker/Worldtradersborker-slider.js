a5d670b6bf659bc16266e90c3e8e97099ec2a067
Wilson <Fishas02@gmail.com>
2024-08-31 20:01:52 -0500

    cambios realizados subiendo informacion en cada proyecto de wordpress

---
+++ ProyectosHTML/Proyecto-Wordpress/Worldtradersbroker/Worldtradersborker-slider.js
@@ -0,0 +1,71 @@
+// variables para slider de img
+let items = document.querySelectorAll('.slider .item');
+let next = document.getElementById('next');
+let prev = document.getElementById('prev');
+
+// variables de menu para mobile
+let menuIcon = document.querySelector('.fa-bars');
+let navbar = document.querySelector('.navbar');
+
+let sections = document.querySelectorAll('section');
+let navlinks = document.querySelectorAll('header nav ul li a');
+
+// codigo para menu mobile
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if(top >= offset && top < offset + height){
+            navlinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector(`header nav ul li a[href*='${id}']`).classList.add('active');
+            })
+        }
+    })
+}
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+}
+
+
+// codigo para funcionamiento de slider
+
+let active = 0;
+function loadShow() {
+    let stt = 0;
+    items[active].style.transform = `none`;
+    items[active].style.zIndex = 1;
+    items[active].style.filter = 'none';
+    items[active].style.opacity = 1;
+    for (let i = active + 1; i < items.length; i++) {
+        stt++;
+        items[i].style.transform = `translateX(${120 * stt}px) scale(${1 - 0.2 * stt}) perspective(16px) rotateY(-1deg)`;
+        items[i].style.zIndex = -stt;
+        items[i].style.filter = 'blur(5px)';
+        items[i].style.opacity = stt > 2 ? 0 : 0.6;
+    }
+    stt = 0;
+    for (let i = active - 1; i >= 0; i--) {
+        stt++;
+        items[i].style.transform = `translateX(${-120 * stt}px) scale(${1 - 0.2 * stt}) perspective(16px) rotateY(1deg)`;
+        items[i].style.zIndex = -stt;
+        items[i].style.filter = 'blur(5px)';
+        items[i].style.opacity = stt > 2 ? 0 : 0.6;
+    }
+}
+loadShow();
+
+next.onclick = function() {
+    active = active + 1 < items.length ? active + 1 : active;
+    loadShow();
+}
+
+prev.onclick = function() {
+    active = active - 1 >= 0 ? active - 1 : active;
+    loadShow();
+}
